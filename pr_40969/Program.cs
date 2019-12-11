56bc55bd44dd21194e22a631c74fbb75b2bbfad8
Grzesiek <grzesiek7393223@o2.pl>
2019-12-11 16:29:17 +0100

    40969

---
+++ pr_40969/Program.cs
@@ -0,0 +1,16 @@
+﻿using Dziedziczenie;
+using System;
+
+namespace pr_40969
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SilnikBenz x = new SilnikBenz(163, 9.5, 90);
+            IPojazd a = new SportovySamochod(x);
+            a.Jedz(10);
+
+        }
+    }
+}
