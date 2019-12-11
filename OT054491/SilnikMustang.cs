a957935d0776e65a6234dc9b76fdbfec8bd133cb
Oswald Toma <oswald.toma@gmail.com>
2019-12-11 20:45:01 +0100

    Zmiana sciezki, przeniesienie klas do osobnych plikow

---
+++ OT054491/SilnikMustang.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Dziedziczenie;
+
+namespace OT054491
+{
+    class SilnikMustang : Silnik
+    {
+        public SilnikMustang(int moc, double spalanie, string dzwiek) : base(moc, spalanie, dzwiek)
+        {
+            if(spalanie < 5)
+                Console.WriteLine("W sam raz dla żony!");
+        }
+
+        public void Dzialaj()
+        {
+            if (this.Moc <= 0)
+            {
+                Console.WriteLine("To przeczy prawom fizyki");
+                base.Dzialaj();
+                Console.WriteLine("Cos sie popsulo");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
