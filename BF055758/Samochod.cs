f6d024baae54677572f6a5feeb0e4c1984ad95df
creativeBernadka <bfryczkowska@gmail.com>
2019-12-13 09:17:41 +0100

    FEAT: add classes SilnikElektryczny and Samochod

---
+++ BF055758/Samochod.cs
@@ -0,0 +1,18 @@
+using Dziedziczenie;
+
+namespace BF055758
+{
+    public class Samochod: IPojazd
+    {
+        
+        public void Jedz()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Jedz(int dystans)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BF055758/SilnikElektryczny.cs
@@ -0,0 +1,11 @@
+using Dziedziczenie;
+
+namespace BF055758
+{
+    public class SilnikElektryczny : Silnik
+    {
+        public SilnikElektryczny(int moc, double spalanie, string dzwiek) : base(moc, 0, "...")
+        {
+        }
+    }
+}
