c081a9f53c427c77121f07fa32b04aad36197eb6
Dominik Katana <dk055906@student.ath.edu.pl>
2019-12-13 08:47:43 +0100

    PT054493

---
+++ PT054493/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PT054493
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
