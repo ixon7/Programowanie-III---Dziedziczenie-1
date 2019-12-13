092cca5b54c1ee5ff30f466dc0f7979660e50483
creativeBernadka <bfryczkowska@gmail.com>
2019-12-13 08:43:56 +0100

    FEAT: add project BF055758 to solution

---
+++ BF055758/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BF055758
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
