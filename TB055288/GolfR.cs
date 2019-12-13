3a12d13f6102f0f0ea900f6a59cea9294857301b
aaa <aa@aa.a>
2019-12-13 10:25:00 +0100

    Labolatorium8

---
+++ TB055288/GolfR.cs
@@ -0,0 +1,33 @@
+﻿using Dziedziczenie;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TB055288
+{
+    public class GolfR : IPojazd
+    {
+        public Silnik silnik { get; set; }
+        public int Przyspieszenie;
+        
+        public void Jedz() => Jedz(150);
+
+        public void Jedz(int dystans)
+        {
+            
+            for (int i = 0; i < dystans; i++)
+            {
+                Przyspieszenie += 2;
+                if (i % 25 == 0)
+                    Console.WriteLine(Przyspieszenie + " km/h");
+                    
+                Console.WriteLine(" ");
+                silnik.Dzialaj();
+            }
+        }
+        public GolfR(TSI Silnik)
+        {
+            silnik = Silnik;
+        }
+    }
+}
---
+++ TB055288/TSI.cs
@@ -0,0 +1,13 @@
+﻿using Dziedziczenie;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TB055288
+{
+    public class TSI : Silnik
+    {
+       
+        public TSI(int moc, int spalanie) : base(spalanie, moc, "Łututu") { }
+    }
+}
