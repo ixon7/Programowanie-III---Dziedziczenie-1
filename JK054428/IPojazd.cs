0aa7b0172bcb93ffe57c70f8d42cc53edb8376f3
Justyna Kowal <justynakowal601@gmail.com>
2019-12-13 10:20:42 +0100

    Lab_8

---
+++ JK054428/IPojazd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JK054428
+{
+    public interface IPojazd
+    {
+        void Jedz();
+
+        void Jedz(int dystans);
+    }
+}
