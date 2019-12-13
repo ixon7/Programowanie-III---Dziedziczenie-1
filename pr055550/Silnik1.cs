9e5b72ee0f2aa9092265f02e5e539a052a18cb62
Mikolaj Siwiec <ms055950@student.ath.edu.pl>
2019-12-13 10:08:02 +0100

    Zmiana20

---
+++ pr055550/Silnik1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dziedziczenie;
+
+namespace pr055550
+{
+    public class Silnik1 : Silnik
+    {
+        public int Glosnosc { get; private set; }
+
+        public Silnik1(int moc, double spalanie, int Glosnosc) : base(moc, spalanie, "bruum") { }
+    }
+}
