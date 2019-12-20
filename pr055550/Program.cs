5c6c672c9f3d89e2d105819ff85452ba4ccc837e
zxsdczs <asdasd@gdsf>
2019-12-20 08:40:44 +0100

    Change

--- pr055550/Program.cs
+++ pr055550/Program.cs
@@ -8,7 +8,7 @@ namespace pr055550
     {
         public static void Main(string[] args)
         {
-            Silnik1 silnik = new Silnik1(5001, 9, 40);
+            Silnik1 silnik = new Silnik1(5000, 9);
             var Kierowca = new Nissan(silnik);
             Kierowca.Jedz(101);
         }
--- pr055550/Silnik1.cs
+++ pr055550/Silnik1.cs
@@ -6,9 +6,7 @@ using Dziedziczenie;
 namespace pr055550
 {
     public class Silnik1 : Silnik
-    {
-        public int Glosnosc { get; private set; }
-
-        public Silnik1(int moc, double spalanie, int Glosnosc) : base(moc, spalanie, "bruum") { }
+    { 
+        public Silnik1(int moc, double spalanie) : base(moc, spalanie, "bruum") { }
     }
 }
