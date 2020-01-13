f1c4e74ed86186e55aa4a1219a5832521472074d
Jakub Salamun <zellanderplx@gmail.com>
2020-01-13 19:17:48 +0100

    pr_054475

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Dziedziczenie;
+using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Silnikinfi a = new Silnikinfi(190, 10);
+            Infinitii b = new Infinitii(a);
+            b.Jedz(10);
+            Console.ReadLine();
+        }
+        
+    }
+}
