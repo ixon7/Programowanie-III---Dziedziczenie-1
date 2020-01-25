5f60d3685c9e486ee7f5ec92e509ec6ab4e4cd0d
miszlon <milosz.szkudlarz@gmail.com>
2020-01-25 13:19:40 +0100

    Komit Interf

---
+++ MS054486/SilnikElektryczny.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dziedziczenie
+{
+    
+        class SilnikElektryczny : Silnik
+        {
+            public int MaksymalnyDystans;
+            public SilnikElektryczny(int moc, double spalanie, string dzwiek) : base(moc, spalanie, dzwiek)
+            {
+                MaksymalnyDystans = 50;
+            }
+        }
+    
+}
