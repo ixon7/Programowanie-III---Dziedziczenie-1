0a7d6c275d1c8a8cab90d0b0447c13146ff22efb
michal666-bit <gajewski.michal88@gmail.com>
2020-01-30 16:01:23 +0100

    PIII-Dziedziczenie P

---
+++ MG54402/Map.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dziedziczenie;
+
+namespace MG54402
+{
+    class Map : Silnik
+    {
+        public Map(int moc, double spalanie) :
+            base(moc, spalanie, "kle kle kle")
+        {
+
+        }
+    }
+}
