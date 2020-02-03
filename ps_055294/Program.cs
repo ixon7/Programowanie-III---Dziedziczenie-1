f76c558d077f04c3cba78d285e715604a9bde66e
Paweł Skalski <40362366+pawelskalski@users.noreply.github.com>
2020-02-03 13:45:19 +0100

    Poprawki

--- ps_055294/Program.cs
+++ ps_055294/Program.cs
@@ -13,56 +13,7 @@ namespace ps_055294
         }
     }
 
-    public class Punciak : IPojazd
-    {
-        public Silnik Silnik { get; private set; }
+    
 
-        public void Jedz() => Jedz(1000);
-
-        public void Jedz(int dystans)
-        {
-            for (int i = 0; i < dystans; i++)
-            {
-                Silnik.Dzialaj();
-                SilnikBenzynowy.zwiekszPrzebieg();
-                if (SilnikBenzynowy.Przebieg1 > 10000)
-                {
-                    Zepsuj();
-                }
-            }
-        }
-
-        public void Zepsuj()
-        {
-            throw new Exception("Punciak Kaput");
-        }
-
-        public Punciak(SilnikBenzynowy silnik)
-        {
-            Silnik = silnik;
-        }
-    }
-
-    public class SilnikBenzynowy : Silnik
-    {
-        private static int Przebieg;
-
-        public SilnikBenzynowy(int moc, double spalanie, string dzwiek, int przebieg) : base(moc, spalanie, dzwiek)
-        {
-            Przebieg = przebieg;
-        }
-
-
-        public static int Przebieg1
-        {
-            get => Przebieg;
-            set => Przebieg = value;
-        }
-
-
-        public static void zwiekszPrzebieg()
-        {
-            Przebieg += 1;
-        }
-    }
+    
 }
