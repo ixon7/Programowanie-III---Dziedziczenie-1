c6ca1aafb975b1c5c84e80c9b666d4fda1f3eb65
ArcyAs <serilmagnifico@gmail.com>
2020-02-20 21:22:19 +0100

    zaległości

---
+++ AK055547/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AK055547
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
