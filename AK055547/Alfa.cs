da62c315672ea3c4a9d677c3d58282045346504f
ArcyAs <serilmagnifico@gmail.com>
2020-02-20 21:36:30 +0100

    zaległości

---
+++ AK055547/Alfa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Dziedziczenie;
+
+namespace AK055547
+{
+    public class Alfa : IPojazd
+    {
+        public Silnik Diesel { get; }
+        public void Jedz() => Jedz(700);
+        public void Jedz(int dystans)
+        {
+            int zasieg = 900;
+            if (dystans < zasieg)
+            {
+                for (int i = 0; i < dystans; i++)
+                {
+                    if (i==zasieg/4)
+                    {
+                        Console.WriteLine(" \nHai il 75% di carburante!");
+                    }
+                    else if(i==zasieg/2)
+                    {
+                        Console.WriteLine("\nMetà del serbatoio!");
+                    }
+                    Diesel.Dzialaj();
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nNessun carburante!");
+            }
+        }
+        public Alfa(JTDm poj19)
+        {
+            Diesel = poj19;
+        }
+    }
+}
---
+++ AK055547/JTDm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Dziedziczenie;
+
+namespace AK055547
+{
+    public class JTDm : Silnik
+    {
+        public JTDm(int moc, double spalanie) : base(moc, spalanie, "kle kle kleee kle kle") {}
+    }
+}
+        
