7a7823ffcfd0f5c4cc52a75dbb7c89dd94fd99ac
ixon7 <55660649+ixon7@users.noreply.github.com>
2020-02-21 21:48:28 +0100

    Add files via upload

---
+++ MC053828/SilnikTuningExtraPlus.cs
@@ -0,0 +1,17 @@
+﻿using Dziedziczenie;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace MS055452
+{
+    class SilnikTuningExtraPlus: Silnik
+    {
+        public SilnikTuningExtraPlus(int moc, double spalanie) :
+            base(moc, spalanie, "WWWWWZZZIU")
+        {
+
+        }
+    }
+}
